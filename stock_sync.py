ae0ddc73293cf2673e6b8dfcdc42297f0a94eebd
aylinnwang <aylinnwang@gmail.com>
2026-06-12 12:37:15 +0800

    Update stock_sync.py

--- stock_sync.py
+++ stock_sync.py
@@ -1,1 +1,8 @@
-print("Notion Stock Sync Running")
+import os
+
+print("Notion同步启动")
+
+notion_token = os.getenv("NOTION_TOKEN")
+database_id = os.getenv("NOTION_DATABASE_ID")
+
+print("Database:", database_id)
